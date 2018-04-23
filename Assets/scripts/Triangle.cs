cc17e6226dbb60b443f47f290ae855337def2120
DarkMoun <elbaf828@gmail.com>
2018-04-23 10:35:21 +0200

    Initial commit

---
+++ Assets/scripts/Triangle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triangle : MonoBehaviour {
+    public class octogone
+    {
+        GameObject triangleun;
+        GameObject triangledeux;
+        GameObject triangletrois;
+        GameObject trianglequatre;
+        GameObject trianglecinq;
+        GameObject trianglesix;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
---
+++ Assets/scripts/tourne_triangles.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class tourne_triangles : MonoBehaviour {
+    public class octogone
+    {
+        GameObject triangleun;
+        GameObject triangledeux;
+        GameObject triangletrois;
+        GameObject trianglequatre;
+        GameObject trianglecinq;
+        GameObject trianglesix;
+    }
+
+
+    public GameObject centre;
+
+
+
+
+
+
+    void Start () {
+        Button btn = centre.GetComponent<Button>();
+        btn.onClick.AddListener(Clique);
+	}
+	
+	void Update () {
+
+	}
+    public void Clique()
+    {
+        transform.Rotate(Vector3.forward * -30);
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        octogone = 
+    }
+
+}
