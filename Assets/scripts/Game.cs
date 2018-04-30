cd556ef79831ded097353de8590e29fe2756e2a3
DarkMoun <elbaf828@gmail.com>
2018-04-30 11:55:03 +0200

    Méthode de calcul modifiée
    Les opérations sont faites par ordre de priorité (* et / d'abord, puis + et -)

--- Assets/scripts/Game.cs
+++ Assets/scripts/Game.cs
@@ -13,9 +13,14 @@ public class Game : MonoBehaviour {
     public List<string> operators;  //list of operators (can be set in editor)
 
     //lists used to calculate
-    private List<float> calculations;
     private List<float> results;
     private List<GameObject> tmpSelected;
+    private List<string> allOperations;
+    private List<string> tmpOperations;
+    private List<string> elements;
+    private Stack<string> calculateLeft;
+    private Stack<string> calculateRight;
+    string currentElem;
 
     // Use this for initialization
     void Start () {
@@ -24,8 +29,11 @@ public class Game : MonoBehaviour {
         buttons = new List<GameObject>();
         selected = new List<GameObject>();
         //operators = new List<string>();
-        calculations = new List<float>();
         results = new List<float>();
+        allOperations = new List<string>();
+        tmpOperations = new List<string>();
+        calculateLeft = new Stack<string>();
+        calculateRight = new Stack<string>();
 
         //put all triangles in "triangles"
         foreach (Triangle tri in this.GetComponentsInChildren<Triangle>())
@@ -70,29 +78,9 @@ public class Game : MonoBehaviour {
                 }
                 else // if the operators list isn't empty
                 {
-                    results.Clear();
-
-                    tmpSelected = new List<GameObject>(selected); //copy the list
-                    //remove the first and the last number of the list
-                    tmpSelected.RemoveAt(nbSelected - 1);
-                    results.Add((float)tmpSelected[0].GetComponent<Triangle>().number); //add the first number to the result list
-                    tmpSelected.RemoveAt(0);
-                    //for each remaining numbers
-                    for (int i = 0; i < nbSelected - 2; i++)
-                    {
-                        calculations.Clear();
-                        int nextNumber = tmpSelected[i].GetComponent<Triangle>().number; //next number to be used to calculate (start from the second in the selected list)
-                        foreach (float res in results)
-                        {
-                            //calculate all possible operations between "nextNumber" and all numbers in result
-                            foreach (string op in operators)
-                            {
-                                calculations.Add(Calculate(res, op, nextNumber));
-                            }
-                        }
-                        //save all new results in the list "results"
-                        results = new List<float>(calculations);
-                    }
+                    //calculate all possible results
+                    GenerateAllOperations(allOperations, selected);
+                    CalculateAllOperations(allOperations, results);
 
                     //when all possible results are calculated, check if the last selected number is among them
                     if (results.Contains((float)last.GetComponent<Triangle>().number))
@@ -127,8 +115,118 @@ public class Game : MonoBehaviour {
         }
 	}
 
+    private void GenerateAllOperations(List<string> allOp, List<GameObject> selectedTriangles)
+    {
+        allOp.Clear();
+        int nbSelected = selectedTriangles.Count;
+        tmpSelected = new List<GameObject>(selectedTriangles); //copy the list
+        //remove the first and the last number of the list
+        tmpSelected.RemoveAt(nbSelected - 1);
+        allOp.Add(tmpSelected[0].GetComponent<Triangle>().number.ToString()); //add the first number to the result list
+        tmpSelected.RemoveAt(0);
+        //for each remaining numbers
+        for (int i = 0; i < nbSelected - 2; i++)
+        {
+            tmpOperations.Clear();
+            int nextNumber = tmpSelected[i].GetComponent<Triangle>().number; //next number to be used to calculate (start from the second in the selected list)
+            foreach (string operation in allOp)
+            {
+                //generate all possible operations between "nextNumber" and all operations in tmpOperations
+                foreach (string op in operators)
+                {
+                    tmpOperations.Add(string.Concat(operation, " ", op, " ", nextNumber.ToString()));
+                }
+            }
+            //save all new operations in the list "allOp"
+            allOp = new List<string>(tmpOperations);
+        }
+    }
+
+    private void CalculateAllOperations(List<string> allOp, List<float> operationResults)
+    {
+        foreach (string operation in allOp)
+        {
+            calculateLeft = new Stack<string>(operation.Split(' ')); //split by ' ' the operation and put all elements in a stack
+            int nbElem;
+            int nbOperators = calculateLeft.Count / 2;
+            for (int i = 0; i < nbOperators; i++) //for each operators
+            {
+                nbElem = calculateLeft.Count; //nb numbers + nb operators
+                //check the presence of each operator by priority
+                if (calculateLeft.Contains("*") || calculateLeft.Contains("/"))
+                {
+                    //browse the list to find "*" or "/"
+                    for (int j = 0; j < nbElem; j++)
+                    {
+                        currentElem = calculateLeft.Pop(); //take an element from the left stack
+                        //if the element is "*" or "/" calculate and put the result in the right stack and stop browsing
+                        if (currentElem == "*" || currentElem == "/")
+                        {
+                            //take one number from each stack
+                            float a, b;
+                            float.TryParse(calculateRight.Pop(), out a);
+                            float.TryParse(calculateLeft.Pop(), out b);
+                            //calculate and put the result in the right stack
+                            calculateRight.Push(Calculate(a, currentElem, b).ToString());
+                            //move all elements from the right stack to the left stack
+                            int nbRight = calculateRight.Count;
+                            for (int k = 0; k < nbRight; k++)
+                            {
+                                calculateLeft.Push(calculateRight.Pop());
+                            }
+                            break; //stop looking for "*" or "/"
+                        }
+                        //else put the element in the right stack and keep browsing
+                        else
+                        {
+                            calculateRight.Push(currentElem);
+                        }
+                    }
+                }
+                else if (calculateLeft.Contains("+") || calculateLeft.Contains("-"))
+                {
+                    //browse the list to find "+" or "-"
+                    for (int j = 0; j < nbElem; j++)
+                    {
+                        currentElem = calculateLeft.Pop(); //take an element from the left stack
+                        //if the element is "+" or "-" calculate and put the result in the right stack and stop browsing
+                        if (currentElem == "+" || currentElem == "-")
+                        {
+                            //take one number from each stack
+                            float a, b;
+                            float.TryParse(calculateRight.Pop(), out a);
+                            float.TryParse(calculateLeft.Pop(), out b);
+                            //calculate and put the result in the right stack
+                            calculateRight.Push(Calculate(a, currentElem, b).ToString());
+                            //move all elements from the right stack to the left stack
+                            int nbRight = calculateRight.Count;
+                            for (int k = 0; k < nbRight; k++)
+                            {
+                                calculateLeft.Push(calculateRight.Pop());
+                            }
+                            break; //stop looking for "+" or "-"
+                        }
+                        //else put the element in the right stack and keep browsing
+                        else
+                        {
+                            calculateRight.Push(currentElem);
+                        }
+                    }
+                }
+                else
+                {
+                    Debug.Log("An operator is in the list but isn't recognized in the code. Add a case \"if calculateLeft.Contains(<operator>)\" in the fonction \"CalculateAllOperations\" of the script \"Game\".");
+                }
+            }
+            //add the result of the operation to the list
+            float result;
+            float.TryParse(calculateLeft.Pop(), out result);
+            operationResults.Add(result);
+        }
+    }
+
     //calculate the operation "a op b" with "op" the operator
-    private float Calculate(float a, string op, int b)
+    private float Calculate(float a, string op, float b)
     {
         float result = Mathf.Infinity;
         switch (op)
